312bf0b2bd3dd90ecc0a40bc35d4fbc621d84fb7
ulegend <shivamchaudhary6197@gmail.com>
2022-03-25 22:52:11 +0530

    Update

--- IPS/settings.py
+++ IPS/settings.py
@@ -140,7 +140,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "/"    # Redirect to home page if next param is not available in GET...
 LOGIN_URL = "registration/login"
-LOGOUT_REDIRECT_URL = "/registration/login"
+LOGOUT_REDIRECT_URL = "/registration/signup"
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
